ac65da21b5525e1e26d260f91bc0844f541dbbd2
Albin Corén <twoten@twoten.io>
2019-08-07 10:31:45 +0200

    Changed query indentation to make the sourcecode easier to read

--- ClientExample/Program.cs
+++ ClientExample/Program.cs
@@ -34,32 +34,33 @@ namespace ClientExample
                 queryConnection.Connect("127.0.0.1", 9423);
 
                 // Send query
-                List<ServerModel> models = queryConnection.SendQuery(@"{
-                                                                            ""$and"": [
-                                                                                {
-                                                                                    ""Players"": {
-                                                                                        ""$gte"": 20
-                                                                                    }
-                                                                                },
-                                                                                {
-                                                                                    ""Players"": {
-                                                                                        ""$lte"": 50
-                                                                                    }
-                                                                                },
-                                                                                {
-                                                                                    ""Players"": {
-                                                                                        ""$in"": [
-                                                                                            12,
-                                                                                            13,
-                                                                                            14,
-                                                                                            23,
-                                                                                            43,
-                                                                                            51
-                                                                                        ]
-                                                                                    }
-                                                                                }
-                                                                            ]
-                                                                        }");
+                List<ServerModel> models = queryConnection.SendQuery(@"
+                {
+                    ""$and"": [
+                        {
+                            ""Players"": {
+                                ""$gte"": 20
+                            }
+                        },
+                        {
+                            ""Players"": {
+                                ""$lte"": 50
+                            }
+                        },
+                        {
+                            ""Players"": {
+                                ""$in"": [
+                                    12,
+                                    13,
+                                    14,
+                                    23,
+                                    43,
+                                    51
+                                ]
+                            }
+                        }
+                    ]
+                }");
 
                 Console.WriteLine(string.Format("| {0,5} | {1,5} | {2,5} |", "UUID", "Name", "Players"));
                 Console.WriteLine(string.Join(Environment.NewLine, models.Select(x => string.Format("| {0,5} | {1,5} | {2,5} |", x.Id, x.ContractData["Name"], x.ContractData["Players"]))));
